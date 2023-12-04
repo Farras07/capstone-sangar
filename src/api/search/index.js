55edd863b3f3efb69735100853459b4c11cda5e1
Agyl Restu Hermanto <agyl1restu@gmail.com>
2023-12-04 21:29:58 +0700

    search endpoint

--- src/api/search/index.js
+++ src/api/search/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const FlowerHandler = require('./handler')
+const SearchHandler = require('./handler')
 const validator = require('../../validator/flowers')
 const FlowerServices = require('../../services/flowerServices')
 const flowerServices = new FlowerServices()
-const handler = new FlowerHandler(flowerServices, validator)
+const handler = new SearchHandler(flowerServices, validator)
 
-router.get('/', async (req, res) => {
+router.get('/:sellerId/flower', async (req, res) => {
   try {
-    const data = await handler.getFlowersHandler()
+    const { sellerId } = req.params
+    const { flowerName } = req.query
+    const data = await handler.getSellerFlowerByNameHandler(flowerName, sellerId)
     res.status(200)
     res.send(
       {
@@ -18,16 +20,19 @@ router.get('/', async (req, res) => {
       }
     )
   } catch (error) {
+    console.error('Error:', error)
     res.status(error.statusCode || 500).send({
       status: 'Fail',
-      message: error.message
+      message: error.message,
+      error: error
     })
   }
 })
-router.get('/:id', async (req, res) => {
+
+router.get('/flowers', async (req, res) => {
   try {
-    const { id } = req.params
-    const data = await handler.getFlowerByIdHandler(id)
+    const { flowerName } = req.query
+    const data = await handler.getFlowerByNameHandler(flowerName)
     res.status(200)
     res.send(
       {
@@ -37,9 +42,11 @@ router.get('/:id', async (req, res) => {
       }
     )
   } catch (error) {
+    console.error('Error:', error)
     res.status(error.statusCode || 500).send({
       status: 'Fail',
-      message: error.message
+      message: error.message,
+      error: error
     })
   }
 })
