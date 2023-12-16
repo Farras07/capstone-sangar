10c781b3b748b8ce0f4fae31dad9aa545c9619c0
Agyl Restu Hermanto <92309945+Qwwn@users.noreply.github.com>
2023-12-16 23:49:11 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const catalog = require('./api/catalog')
 
 dotenv.config({ path: '.env.development' })
 const host = process.env.HOST
-const port = process.env.port
+const port = process.env.port || 3000
 
 app.use(bodyParser.json())
 
